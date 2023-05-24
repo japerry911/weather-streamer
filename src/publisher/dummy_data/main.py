0d99318eb60a805ac3aff62a9d6aec73adc38247
Jack Perry <jack.e.perry@outlook.com>
2023-05-24 08:57:57 -0600

    📦 NEW: added basic streamer set up

--- src/publisher/dummy_data/main.py
+++ src/publisher/dummy_data/main.py
@@ -1,3 +1,4 @@
+import logging
 from time import sleep
 
 from faker import Faker
@@ -5,17 +6,29 @@ from google.cloud import pubsub_v1
 
 
 def main():
-    messages = generate_messages(num=1)
-    publish_messages(messages=messages)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s :: %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    logger.info("Generating messages")
+    messages = generate_messages(num=1_000)
+
+    logger.info("Publishing messages")
+    publish_messages(messages=messages, logger=logger)
 
 
-def publish_messages(messages: list[str]):
+def publish_messages(messages: list[str], logger: logging.Logger):
     publisher = pubsub_v1.PublisherClient()
     topic_name = "projects/weather-streamer/topics/weather-streamer"
 
-    for message in messages:
+    for idx, message in enumerate(messages):
+        logger.info(f"Publishing Message {idx + 1}/{len(messages)}")
         future = publisher.publish(topic_name, message.encode("utf-8"))
         future.result()
+        sleep(3)
 
 
 def generate_messages(num: int) -> list[str]:
@@ -24,7 +37,6 @@ def generate_messages(num: int) -> list[str]:
 
     for _ in range(num):
         messages.append(str(fake.numerify()))
-        sleep(3)
 
     return messages
 
